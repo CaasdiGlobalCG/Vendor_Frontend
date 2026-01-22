7e77fa5e803b6f8db5e56488b2f31f6bae047877
Dhanush P <91492303+dhanush9686@users.noreply.github.com>
2026-01-22 17:54:19 +0530

    Removed copilot-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { useEffect, useContext } from "react";
 
 import HomePage from "./pages/HomePage";
