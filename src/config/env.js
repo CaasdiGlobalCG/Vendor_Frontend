f84b995c1e7fcb7ace6b16946997e1c7c76d50ea
Salman A <steave1233@gmail.com>
2026-01-30 11:03:50 +0530

    adding button for graviyx redirection in both vendor dash as well as crm

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,6 @@
 const PROD_FRONTEND_BASE_URL = 'https://www.caasdiglobal.in';
 const DEV_FRONTEND_BASE_URL = 'http://localhost:5173';
+const DEFAULT_B2B_MARKETPLACE_BASE_URL = 'https://www.graviyx.com';
 
 const resolveEnvValue = (value) => {
   if (typeof value !== 'string') {
@@ -20,9 +21,12 @@ export const VENDOR_BACKEND_URL = ''; // Empty string = relative paths
 
 const resolvedClientUrl = resolveEnvValue(import.meta.env.VITE_CLIENT_URL) || '';
 const resolvedSalesUrl = resolveEnvValue(import.meta.env.VITE_SALES_URL) || '';
+const resolvedB2BMarketplaceBaseUrl =
+  resolveEnvValue(import.meta.env.VITE_B2B_MARKETPLACE_URL) || DEFAULT_B2B_MARKETPLACE_BASE_URL;
 
 export const CLIENT_URL = resolvedClientUrl;
 export const SALES_URL = resolvedSalesUrl;
+export const B2B_MARKETPLACE_URL = `${resolvedB2BMarketplaceBaseUrl.replace(/\/+$/, '')}/home`;
 
 
 const config = {
@@ -30,6 +34,7 @@ const config = {
   VENDOR_BACKEND_URL,
   SALES_URL,
   CLIENT_URL,
+  B2B_MARKETPLACE_URL,
 };
 
 export default config;
